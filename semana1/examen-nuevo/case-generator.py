b9a5d408606779d45105f56eddaa71096a410525
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2021-10-03 23:01:20 -0500

    Examen nuevo (Emilio cantu)

---
+++ semana1/examen-nuevo/case-generator.py
@@ -0,0 +1,31 @@
+import random
+import os
+import itertools as it
+from math import *
+
+baseString = """
+{n}
+{m}
+{calis}
+""".strip(' \t\n\r')
+
+cases = 10
+
+for i in range(cases):
+    caseName = '{}.in'.format(i)
+
+    n = random.randint(1, 10**i)
+    m = random.randint(1, 10**((i+1) // 2))
+
+    calis = [str(random.randint(50, 100) / 10.0) for _ in range(2*m)]
+
+    case = {
+        'n': n,
+        'm': m,
+        'calis': "\n".join(calis)
+    }
+
+    caseString = baseString.format(**case)
+    casePath = os.path.join('cases', caseName)
+    with open(casePath, 'w') as f:
+        f.write(caseString)
