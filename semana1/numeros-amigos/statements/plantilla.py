131ae40ad90e6cd49abd00d2321b2c9c202643c7
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2021-10-07 22:36:25 -0500

    Agregar problema numeros amigos

---
+++ semana1/numeros-amigos/statements/plantilla.py
@@ -0,0 +1,21 @@
+# Leemos los números
+A =
+B =
+# Definimos las variables para guardar la
+# suma de los divisores impropios
+sumdivpA = 0
+sumdivpB = 0
+
+# Buscamos los divisores propios de A
+for a in range():
+    if : # ¿Cómo preguntamos si a es divisor propio de A
+
+# Buscamos los divisores propios de B
+for b in range():
+    if:  # ¿Cómo preguntamos si b es divisor propio de B
+
+
+if  and : # ¿Cuáles son las dos condiciones para que A y B sean amigos?
+    print("Son amigos")
+else:
+    print("No son amigos")
