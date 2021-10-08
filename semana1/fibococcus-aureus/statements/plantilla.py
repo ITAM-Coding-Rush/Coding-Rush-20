65fdc70072548982b001013181ba724e903ea32a
ByJavier1403 <javierdavid02@gmail.com>
2021-10-08 13:14:33 -0500

    Se agregó el problema Fibococcus aureus de la 1er semana

---
+++ semana1/fibococcus-aureus/statements/plantilla.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+#¿En qué valores debemos de iniciar las variables?
+a = 
+b = 
+c = 
+
+#¿Cuántas iteraciones haremos?
+for i in range():
+
+#¿Qué valores deberán de tomar las variables en cada iteración?
+    c=
+    a=
+    b=
+    
+#¿Qué vamos a imprimir como respuesta? 
+print()
