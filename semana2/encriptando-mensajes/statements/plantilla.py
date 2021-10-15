3284edf7db8ba2747a20ef5ce3b9eb0f571429bd
Yosshua Cisneros <42878951+YOSSHUA@users.noreply.github.com>
2021-10-15 10:41:15 -0500

    Update plantilla.py

--- semana2/encriptando-mensajes/statements/plantilla.py
+++ semana2/encriptando-mensajes/statements/plantilla.py
@@ -1,23 +1,23 @@
-# Cómo obtines n?
+# Cómo obtienes n?
 n = 
 
 # Un ciclo for al rededor de qué?
 for :
   
-  # como sabes de que persona es el mensaje?
+  # cómo sabes de que persona es el mensaje?
 	persona = 
-  # como obtienes el mensaje=
+  # cómo obtienes el mensaje=
 	frase =
   
-  # como inicializas el nuevo mensaje?
+  # cómo inicializas el nuevo mensaje?
 	nueva_frase = 
   
-  # como sabes si el mensaje es de lorena?
+  # cómo sabes si el mensaje es de lorena?
 	if persona == :
-    # como recorres caracter a caracter un string?
+    # cómo recorres caracter a caracter un string?
 		for c:
       #ya sabes que tienes que cambiar las letras, cómo cambias las letras a partir del numero que escogió lorena?
-      # que ocurre con las ultimas letras cuando llegas cerca de la z?
+      # qué ocurre con las ultimas letras cuando llegas cerca de la z?
 			if  97 <= ord(c) <= 122:
 				
 			#cómo agragas los espacios?
@@ -25,7 +25,7 @@ for :
 				nueva_frase +=
 		print(nueva_frase)
   
-  # como sabes si el mensaje es de limon?
+  # cómo sabes si el mensaje es de limon?
 	elif persona == :
 		# Lo mismo que haces para lorena pero aplicado a el recorrido de limon
 		print(nueva_frase)
